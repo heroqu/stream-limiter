08c363d4c2ec11cd0805fba02f34d97b0ffc990c
Heroqu <hero.qu@gmail.com>
2020-02-21 00:37:28 +0300

    add prettier, eslint and some npm scripts

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-const { Transform } = require('readable-stream')
+const { Transform } = require('readable-stream');
 
-module.exports = StreamLimiter
+function StreamLimiter(maxBytes) {
+  const sl = new Transform();
 
-function StreamLimiter (maxBytes) {
-  let sl = new Transform()
+  function quit() {
+    sl.push(null); // output stream will know we quit here
+    sl.emit('finish'); // input stream will know
+  }
 
-  let count = 0 // number of bytes passed through
+  let count = 0; // number of bytes passed through
 
-  sl._transform = function (chunk, encoding, cb) {
+  /* eslint no-underscore-dangle: ["error", { "allow": ["_transform"] }] */
+  sl._transform = function _transform(chunk, encoding, cb) {
     if (count >= maxBytes) {
       // the quota is over, quit
-      quit()
-      cb()
-      return
+      quit();
+      cb();
+      return;
     }
 
-    if (chunk.length > (maxBytes - count)) {
+    if (chunk.length > maxBytes - count) {
       // Cut the chunk to respect the quota
-      chunk = chunk.slice(0, maxBytes - count)
+      chunk = chunk.slice(0, maxBytes - count);
     }
 
-    count += chunk.length
-    sl.push(chunk)
+    count += chunk.length;
+    sl.push(chunk);
 
     if (count >= maxBytes) {
       // the quota is over, quit
-      quit()
+      quit();
     }
 
-    cb()
-  }
+    cb();
+  };
 
-  function quit () {
-    sl.push(null)       // output stream will know we quit here
-    sl.emit('finish')   // input stream will know
-  }
-
-  return sl
+  return sl;
 }
+
+module.exports = StreamLimiter;
--- test/stream_accumulator.js
+++ test/stream_accumulator.js
@@ -1,28 +1,28 @@
-const { Writable } = require('readable-stream')
+const { Writable } = require('readable-stream');
 
-module.exports = StreamAccumulator
+module.exports = StreamAccumulator;
 
-function StreamAccumulator () {
+function StreamAccumulator() {
   let sa = new Writable({
-    objectMode: false // object mode is not supported
-  })
+    objectMode: false, // object mode is not supported
+  });
 
   // will accumulate incoming bytes here
-  let buf = Buffer.alloc(0) // start with empty buffer
+  let buf = Buffer.alloc(0); // start with empty buffer
 
-  sa._write = function (chunk, encoding, cb) {
+  sa._write = function(chunk, encoding, cb) {
     if (!(chunk instanceof Buffer)) {
       // convert to a Buffer
-      chunk = Buffer.from(chunk)
+      chunk = Buffer.from(chunk);
     }
-    buf = Buffer.concat([buf, chunk]) // add bytes
-    cb()
-  }
+    buf = Buffer.concat([buf, chunk]); // add bytes
+    cb();
+  };
 
   // getter for the result
-  sa.buffer = function(){
-    return Buffer.from(buf)
-  }
+  sa.buffer = function() {
+    return Buffer.from(buf);
+  };
 
-  return sa
+  return sa;
 }
